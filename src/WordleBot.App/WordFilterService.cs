160f26a51f41ba1f43aa4b627ee6074d33b85dc7
matt-condon <matt_condon@dell.com>
2022-01-06 02:31:09 +0000

    feat: implement list repo, sorting, and initial filtering

---
+++ src/WordleBot.App/WordFilterService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WordleBot.App
+{
+    public class WordFilterService
+    {
+        public List<string> FilterWordsBasedOnResult(
+            List<string> words,
+            char[] incorrectChars,
+            Dictionary<int, char> correctCharsInPosition,  
+            Dictionary<int, char> correctCharsOutOfPosition)
+        {
+            var includedList = new List<string>();
+            var excludedList = new List<string>(); //for testing
+            foreach(string word in words)
+            {
+                if (word.Intersect(incorrectChars).Any())
+                {
+                    excludedList.Add(word);
+                }
+                else
+                {
+                    for (int index = 0; index < 5; index++)
+                    {
+                        var curChar = word[index];
+                        if (correctCharsInPosition.TryGetValue(index, out char resChar) && (resChar != curChar))
+                        {
+                            excludedList.Add(word);
+                            break;
+                        }
+                        if (correctCharsOutOfPosition.TryGetValue(index, out resChar) && resChar == curChar)
+                        {
+                            excludedList.Add(word);
+                            break;
+                        }
+                        if (index == 4)
+                        {
+                            includedList.Add(word);
+                        }
+                    }
+                }
+            }
+            return includedList;
+        }
+    }
+}
---
+++ src/WordleBot.App/WordListRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace WordleBot.App
+{
+    public class WordListRepository
+    {
+        public List<string> GetAllPossibleWordsFromFile()
+        {
+            try
+            {
+                using (var reader = new StreamReader("~\\..\\..\\..\\..\\..\\..\\all-5-letter-words.txt"))
+                {
+                    List<string> listOfWords = new List<string>();
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        listOfWords.Add(line);
+                    }
+                    return listOfWords;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error occurred parsing file, exception {e}");
+            }
+            return new List<string>();
+        }
+    }
+}
